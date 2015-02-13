fb4ffcad1562168007545ffe4e7d550febb75f0a
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-02-13 18:54:44 +0000

    Added FailJSendResponse<T>

---
+++ src/JSendWebApi/Responses/FailJSendResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JSendWebApi.Responses
+{
+    public class FailJSendResponse<T>
+    {
+        private readonly T _data;
+
+        public FailJSendResponse(T data)
+        {
+            if (data == null) throw new ArgumentNullException("data");
+            _data = data;
+        }
+
+        [JsonProperty("status", Order = 1)]
+        public string Status
+        {
+            get { return "fail"; }
+        }
+
+        [JsonProperty("data", Order = 2)]
+        public T Data
+        {
+            get { return _data; }
+        }
+    }
+}
---
+++ tests/JSendWebApi.Tests/Responses/FailJSendResponseTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using JSendWebApi.Responses;
+using JSendWebApi.Tests.FixtureCustomizations;
+using JSendWebApi.Tests.TestTypes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+
+namespace JSendWebApi.Tests.Responses
+{
+    public class FailJSendResponseTests
+    {
+        [Fact]
+        public void ThrowsIfDataIsNull()
+        {
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => new FailJSendResponse<Model>(null));
+        }
+
+        [Theory, JSendAutoData]
+        public void StatisIsFail(FailJSendResponse<Model> response)
+        {
+            // Exercise system and verify outcome
+            response.Status.Should().Be("fail");
+        }
+
+        [Theory, JSendAutoData]
+        public void SerializesCorrectly([Frozen] Model model, FailJSendResponse<Model> response)
+        {
+            // Fixture setup
+            var expectedSerializedResponse = new JObject
+            {
+                {"status", "fail"},
+                {"data", JObject.FromObject(model)}
+            };
+            // Exercise system
+            var serializedResponse = JObject.FromObject(response);
+            // Verify outcome
+            JToken.DeepEquals(serializedResponse, expectedSerializedResponse)
+                .Should().BeTrue();
+        }
+    }
+}
