336a929c7fda849739876289c4364d5348fbe59f
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-02-16 18:06:26 +0000

    Added JSendInternalServerErrorResult

---
+++ src/JSendWebApi/Results/JSendInternalServerErrorResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using JSendWebApi.Responses;
+
+namespace JSendWebApi.Results
+{
+    public class JSendInternalServerErrorResult : IHttpActionResult
+    {
+        private readonly JSendResult<ErrorJSendResponse> _result;
+
+        public JSendInternalServerErrorResult(JSendApiController controller, string message)
+        {
+            _result = new JSendResult<ErrorJSendResponse>(
+                controller, new ErrorJSendResponse(message), HttpStatusCode.InternalServerError);
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return _result.ExecuteAsync(cancellationToken);
+        }
+    }
+}
---
+++ tests/JSendWebApi.Tests/Results/JSendInternalServerErrorResultTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using FluentAssertions;
+using JSendWebApi.Responses;
+using JSendWebApi.Results;
+using JSendWebApi.Tests.FixtureCustomizations;
+using Newtonsoft.Json;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit;
+using Xunit.Extensions;
+
+namespace JSendWebApi.Tests.Results
+{
+    public class JSendInternalServerErrorResultTests
+    {
+        [Theory, JSendAutoData]
+        public void IsHttpActionResult(JSendInternalServerErrorResult result)
+        {
+            // Exercise system and verify outcome
+            result.Should().BeAssignableTo<IHttpActionResult>();
+        }
+
+        [Theory, JSendAutoData]
+        public void ConstructorsThrownWhenAnyArgumentIsNull(GuardClauseAssertion assertion)
+        {
+            // Exercise system and verify outcome
+            assertion.Verify(typeof (JSendInternalServerErrorResult).GetConstructors());
+        }
+
+        [Theory, JSendAutoData]
+        public async Task ReturnsErrorJSendMessage([Frozen] string errorMessage, JSendInternalServerErrorResult result)
+        {
+            // Fixture setup
+            var jsendError = JsonConvert.SerializeObject(new ErrorJSendResponse(errorMessage));
+            // Exercise system
+            var message = await result.ExecuteAsync(new CancellationToken());
+            // Verify outcome
+            var content = await message.Content.ReadAsStringAsync();
+            content.Should().Be(jsendError);
+        }
+
+        [Theory, JSendAutoData]
+        public async Task StatusCodeIs500(JSendInternalServerErrorResult result)
+        {
+            // Exercise system
+            var message = await result.ExecuteAsync(new CancellationToken());
+            // Verify outcome
+            message.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        }
+
+        [Theory, JSendAutoData]
+        public async Task SetsCharSetHeader(IFixture fixture)
+        {
+            // Fixture setup
+            var encoding = Encoding.ASCII;
+            fixture.Inject(encoding);
+
+            var result = fixture.Create<JSendInternalServerErrorResult>();
+            // Exercise system
+            var message = await result.ExecuteAsync(new CancellationToken());
+            // Verify outcome
+            message.Content.Headers.ContentType.CharSet.Should().Be(encoding.WebName);
+        }
+
+        [Theory, JSendAutoData]
+        public async Task SetsContentTypeHeader(JSendInternalServerErrorResult result)
+        {
+            // Exercise system
+            var message = await result.ExecuteAsync(new CancellationToken());
+            // Verify outcome
+            message.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+        }
+    }
+}
