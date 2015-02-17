07337f3f0855e6089cedeca167c763e82b5c1ee9
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-02-17 18:25:10 +0000

    Added JSendNotFoundResult

---
+++ src/JSendWebApi/Results/JSendNotFoundResult.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using JSendWebApi.Responses;
+
+namespace JSendWebApi.Results
+{
+    public class JSendNotFoundResult : IHttpActionResult
+    {
+        private readonly JSendResult<FailJSendResponse> _result;
+
+        public JSendNotFoundResult(JSendApiController controller, string reason)
+        {
+            if (reason == null)
+                throw new ArgumentNullException("reason");
+
+            if (string.IsNullOrWhiteSpace(reason))
+                throw new ArgumentException("Reason cannot be an empty string.", "reason");
+
+            _result = new JSendResult<FailJSendResponse>(
+                controller, new FailJSendResponse(reason), HttpStatusCode.NotFound);
+        }
+
+        public FailJSendResponse Response
+        {
+            get { return _result.Response; }
+        }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return _result.ExecuteAsync(cancellationToken);
+        }
+    }
+}
---
+++ tests/JSendWebApi.Tests/Results/JSendNotFoundResultTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using FluentAssertions;
+using JSendWebApi.Responses;
+using JSendWebApi.Results;
+using JSendWebApi.Tests.FixtureCustomizations;
+using Newtonsoft.Json;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+
+namespace JSendWebApi.Tests.Results
+{
+    public class JSendNotFoundResultTests
+    {
+        [Theory, JSendAutoData]
+        public void IsHttpActionResult(JSendNotFoundResult result)
+        {
+            // Exercise system and verify outcome
+            result.Should().BeAssignableTo<IHttpActionResult>();
+        }
+
+        [Theory, JSendAutoData]
+        public void ConstructorsThrowWhenAnyArgumentIsNull(GuardClauseAssertion assertion)
+        {
+            // Exercise system and verify outcome
+            assertion.Verify(typeof (JSendNotFoundResult).GetConstructors());
+        }
+
+        [Theory, JSendAutoData]
+        public void ConstructorThrowsWhenReasonIsWhiteSpace(JSendApiController controller)
+        {
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentException>(() => new JSendNotFoundResult(controller, "  "));
+        }
+
+        [Theory, JSendAutoData]
+        public void ResponseIsInitialized(JSendNotFoundResult result)
+        {
+            // Exercise system and verify outcome
+            result.Response.Should().NotBeNull();
+        }
+
+        [Theory, JSendAutoData]
+        public void ResponseIsFail(JSendNotFoundResult result)
+        {
+            // Exercise system and verify outcome
+            result.Response.Should().BeAssignableTo<FailJSendResponse>();
+        }
+
+        [Theory, JSendAutoData]
+        public async Task ResponseIsSerializedIntoBody(JSendNotFoundResult result)
+        {
+            // Fixture setup
+            var serializedResponse = JsonConvert.SerializeObject(result.Response);
+            // Exercise system
+            var message = await result.ExecuteAsync(new CancellationToken());
+            // Verify outcome
+            var content = await message.Content.ReadAsStringAsync();
+            content.Should().Be(serializedResponse);
+        }
+
+        [Theory, JSendAutoData]
+        public void ResponseDataIsCorrectlySet([Frozen] string reason, JSendNotFoundResult result)
+        {
+            // Exercise system and verify outcome
+            result.Response.Data.Should().Be(reason);
+        }
+
+        [Theory, JSendAutoData]
+        public async Task StatusCodeIs404(JSendNotFoundResult result)
+        {
+            // Exercise system
+            var message = await result.ExecuteAsync(new CancellationToken());
+            // Verify outcome
+            message.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Theory, JSendAutoData]
+        public async Task SetsCharSetHeader(IFixture fixture)
+        {
+            // Fixture setup
+            var encoding = Encoding.ASCII;
+            fixture.Inject(encoding);
+
+            var result = fixture.Create<JSendNotFoundResult>();
+            // Exercise system
+            var message = await result.ExecuteAsync(new CancellationToken());
+            // Verify outcome
+            message.Content.Headers.ContentType.CharSet.Should().Be(encoding.WebName);
+        }
+
+        [Theory, JSendAutoData]
+        public async Task SetsContentTypeHeader(JSendNotFoundResult result)
+        {
+            // Exercise system
+            var message = await result.ExecuteAsync(new CancellationToken());
+            // Verify outcome
+            message.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+        }
+    }
+}
