0e5600e996c4ff4e9421e242416a10d2123dc7a8
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-02-19 11:25:22 +0000

    Changed JSendMediaTypeFormatter to format uncaught exceptions thrown by actions

--- src/JSendWebApi/JSendMediaTypeFormatter.cs
+++ src/JSendWebApi/JSendMediaTypeFormatter.cs
@@ -17,9 +17,21 @@ namespace JSendWebApi
         public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content,
             TransportContext transportContext)
         {
-            value = new SuccessJSendResponse(value);
+            value = ConvertValueToJSendResponse(value);
 
             return base.WriteToStreamAsync(type, value, writeStream, content, transportContext);
+        }
+
+        private static object ConvertValueToJSendResponse(object value)
+        {
+            var error = value as HttpError;
+            if (error == null)
+                return new SuccessJSendResponse(value);
+
+            if (!string.IsNullOrWhiteSpace(error.ExceptionMessage))
+                return new ErrorJSendResponse(message: error.ExceptionMessage, data: error);
+
+            return new ErrorJSendResponse(message: error.Message);
         }
     }
 }
--- tests/JSendWebApi.Tests/JSendMediaTypeFormatterTests.cs
+++ tests/JSendWebApi.Tests/JSendMediaTypeFormatterTests.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using FluentAssertions;
 using JSendWebApi.Responses;
 using JSendWebApi.Tests.FixtureCustomizations;
@@ -33,6 +34,44 @@ namespace JSendWebApi.Tests
             {
                 var content = await sreader.ReadToEndAsync();
                 content.Should().Be(jsendSuccess);
+            }
+        }
+
+        [Theory, JSendAutoData]
+        public async Task WrapsHttpErrorsWithExceptionDetailsInErrorJSendResponse(
+            Exception ex, [NoAutoProperties] MemoryStream ms, HttpContent httpContent, TransportContext context,
+            [NoAutoProperties] JSendMediaTypeFormatter formatter)
+        {
+            // Fixture setup
+            var httpError = new HttpError(ex, includeErrorDetail: true);
+            var jsendError = JsonConvert.SerializeObject(new ErrorJSendResponse(ex.Message, httpError));
+            // Exercise system
+            await formatter.WriteToStreamAsync(httpError.GetType(), httpError, ms, httpContent, context);
+            // Verify outcome
+            ms.Position = 0;
+            using (var sreader = new StreamReader(ms))
+            {
+                var content = await sreader.ReadToEndAsync();
+                content.Should().Be(jsendError);
+            }
+        }
+
+        [Theory, JSendAutoData]
+        public async Task WrapsHttpErrorsWithoutExceptionDetailsInErrorJSendResponse(
+            Exception ex, [NoAutoProperties] MemoryStream ms, HttpContent httpContent, TransportContext context,
+            [NoAutoProperties] JSendMediaTypeFormatter formatter)
+        {
+            // Fixture setup
+            var httpError = new HttpError(ex, includeErrorDetail: false);
+            var jsendError = JsonConvert.SerializeObject(new ErrorJSendResponse(httpError.Message));
+            // Exercise system
+            await formatter.WriteToStreamAsync(httpError.GetType(), httpError, ms, httpContent, context);
+            // Verify outcome
+            ms.Position = 0;
+            using (var sreader = new StreamReader(ms))
+            {
+                var content = await sreader.ReadToEndAsync();
+                content.Should().Be(jsendError);
             }
         }
     }
