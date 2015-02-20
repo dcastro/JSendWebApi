665a7d882a90e7601131fa5b13f4b7ad2b585087
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-02-20 09:35:59 +0000

    Added constructor to JSendOkResult that doesn't require a controller

--- src/JSendWebApi/Results/JSendOkResult.cs
+++ src/JSendWebApi/Results/JSendOkResult.cs
@@ -1,9 +1,12 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 using JSendWebApi.Responses;
+using Newtonsoft.Json;
 
 namespace JSendWebApi.Results
 {
@@ -13,7 +16,21 @@ namespace JSendWebApi.Results
 
         public JSendOkResult(JSendApiController controller)
         {
-            _result = new JSendResult<SuccessJSendResponse>(controller, new SuccessJSendResponse(), HttpStatusCode.OK);
+            if (controller == null) throw new ArgumentNullException("controller");
+
+            _result = InitializeResult(controller.JsonSerializerSettings, controller.Encoding, controller.Request);
+        }
+
+        public JSendOkResult(JsonSerializerSettings settings, Encoding encoding, HttpRequestMessage request)
+        {
+            _result = InitializeResult(settings, encoding, request);
+        }
+
+        private JSendResult<SuccessJSendResponse> InitializeResult(JsonSerializerSettings settings, Encoding encoding,
+            HttpRequestMessage request)
+        {
+            return new JSendResult<SuccessJSendResponse>(settings, encoding, request, new SuccessJSendResponse(),
+                HttpStatusCode.OK);
         }
 
         public SuccessJSendResponse Response
