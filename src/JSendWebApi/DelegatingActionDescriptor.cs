253905b75848238e74f485697233523c522e37c5
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-02-20 17:53:19 +0000

    Added DelegatingActionDescriptor

---
+++ src/JSendWebApi/DelegatingActionDescriptor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace JSendWebApi
+{
+    public class DelegatingActionDescriptor : HttpActionDescriptor
+    {
+        private readonly HttpActionDescriptor _descriptor;
+        private readonly IActionResultConverter _resultConverter;
+
+        public DelegatingActionDescriptor(HttpActionDescriptor descriptor, IActionResultConverter resultConverter)
+        {
+            if (descriptor == null) throw new ArgumentNullException("descriptor");
+            if (resultConverter == null) throw new ArgumentNullException("resultConverter");
+
+            _descriptor = descriptor;
+            _resultConverter = resultConverter;
+        }
+
+        public override IActionResultConverter ResultConverter
+        {
+            get { return _resultConverter; }
+        }
+
+        public override Collection<HttpParameterDescriptor> GetParameters()
+        {
+            return _descriptor.GetParameters();
+        }
+
+        public override Task<object> ExecuteAsync(HttpControllerContext controllerContext, IDictionary<string, object> arguments, CancellationToken cancellationToken)
+        {
+            return _descriptor.ExecuteAsync(controllerContext, arguments, cancellationToken);
+        }
+
+        public override string ActionName
+        {
+            get { return _descriptor.ActionName; }
+        }
+
+        public override Type ReturnType
+        {
+            get { return _descriptor.ReturnType; }
+        }
+    }
+}
---
+++ tests/JSendWebApi.Tests/DelegatingActionDescriptorTests.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using FluentAssertions;
+using JSendWebApi.Tests.FixtureCustomizations;
+using Moq;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit;
+using Xunit.Extensions;
+
+namespace JSendWebApi.Tests
+{
+    public class DelegatingActionDescriptorTests
+    {
+        [Theory, JSendAutoData]
+        public void IsHttpActionDescriptor(DelegatingActionDescriptor descriptor)
+        {
+            // Exercise system and verify outcome
+            descriptor.Should().BeAssignableTo<HttpActionDescriptor>();
+        }
+
+        [Theory, JSendAutoData]
+        public void ConstructorsThrowWhenAnyArgumentIsNull(GuardClauseAssertion assertion)
+        {
+            // Exercise system and verify outcome
+            assertion.Verify(typeof (DelegatingActionDescriptor).GetConstructors());
+        }
+
+        [Theory, JSendAutoData]
+        public void InitializesConverterCorrectly(HttpActionDescriptor wrappedDescriptor,
+            IActionResultConverter converter)
+        {
+            // Exercise system
+            var descriptor = new DelegatingActionDescriptor(wrappedDescriptor, converter);
+            // Verify outcome
+            descriptor.ResultConverter.Should().BeSameAs(converter);
+        }
+
+        [Theory, JSendAutoData]
+        public void DelegatesGetParameters([Frozen] HttpActionDescriptor wrappedDescriptor,
+            DelegatingActionDescriptor descriptor)
+        {
+            // Exercise system
+            var result = descriptor.GetParameters();
+            // Verify outcome
+            result.Should().BeSameAs(wrappedDescriptor.GetParameters());
+        }
+
+        [Theory, JSendAutoData]
+        public void DelegatesExecuteAsync(
+            [NoAutoProperties] HttpControllerContext context, IDictionary<string, object> args, CancellationToken token,
+            [Frozen] HttpActionDescriptor wrappedDescriptor, DelegatingActionDescriptor descriptor)
+        {
+            // Exercise system
+            var result = descriptor.ExecuteAsync(null, args, token);
+            // Verify outcome
+            result.Should().BeSameAs(wrappedDescriptor.ExecuteAsync(context, args, token));
+        }
+
+        [Theory, JSendAutoData]
+        public void DelegatesActionName([Frozen] HttpActionDescriptor wrappedDescriptor,
+            DelegatingActionDescriptor descriptor)
+        {
+            // Exercise system
+            var result = descriptor.ActionName;
+            // Verify outcome
+            result.Should().BeSameAs(wrappedDescriptor.ActionName);
+        }
+
+        [Theory, JSendAutoData]
+        public void DelegatesReturnType([Frozen] HttpActionDescriptor wrappedDescriptor,
+            DelegatingActionDescriptor descriptor)
+        {
+            // Exercise system
+            var result = descriptor.ReturnType;
+            // Verify outcome
+            result.Should().BeSameAs(wrappedDescriptor.ReturnType);
+        }
+    }
+}
