4db009dea64b58bd4b4f64ecc39734004349091a
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-02-20 13:52:04 +0000

    Added JSendVoidResultConverter

---
+++ src/JSendWebApi/JSendVoidResultConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using JSendWebApi.Results;
+using Newtonsoft.Json;
+
+namespace JSendWebApi
+{
+    public class JSendVoidResultConverter : IActionResultConverter
+    {
+        private readonly JsonSerializerSettings _serializerSettings;
+        private readonly Encoding _encoding;
+
+        public JSendVoidResultConverter(JsonSerializerSettings serializerSettings, Encoding encoding)
+        {
+            _serializerSettings = serializerSettings;
+            _encoding = encoding;
+        }
+
+        public HttpResponseMessage Convert(HttpControllerContext controllerContext, object actionResult)
+        {
+            if (controllerContext == null)
+                throw new ArgumentNullException("controllerContext");
+
+            var result = new JSendOkResult(_serializerSettings, _encoding, controllerContext.Request);
+
+            return result.ExecuteAsync(CancellationToken.None).Result;
+        }
+    }
+}
