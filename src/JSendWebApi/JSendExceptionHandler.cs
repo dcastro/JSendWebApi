6e49f6097274789f3e0af64b6049f4c4d7a6fd26
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-02-20 00:08:57 +0000

    Added JSendExceptionHandler

---
+++ src/JSendWebApi/JSendExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using JSendWebApi.Results;
+using Newtonsoft.Json;
+
+namespace JSendWebApi
+{
+    public class JSendExceptionHandler : ExceptionHandler
+    {
+        private readonly JsonSerializerSettings _serializerSettings;
+        private readonly Encoding _encoding;
+
+        public JSendExceptionHandler(JsonSerializerSettings serializerSettings, Encoding encoding)
+        {
+            if (serializerSettings == null) throw new ArgumentNullException("serializerSettings");
+            if (encoding == null) throw new ArgumentNullException("encoding");
+
+            _serializerSettings = serializerSettings;
+            _encoding = encoding;
+        }
+
+        public override void Handle(ExceptionHandlerContext context)
+        {
+            var includeErrorDetail = context.Request.ShouldIncludeErrorDetail();
+
+            context.Result = new JSendExceptionResult(
+                includeErrorDetail, _serializerSettings, _encoding, context.Request, context.Exception, null, null, null);
+        }
+    }
+}
---
+++ tests/JSendWebApi.Tests/JSendExceptionHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using FluentAssertions;
+using JSendWebApi.Results;
+using JSendWebApi.Tests.FixtureCustomizations;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Idioms;
+using Xunit.Extensions;
+
+namespace JSendWebApi.Tests
+{
+    public class JSendExceptionHandlerTests
+    {
+        [Theory, JSendAutoData]
+        public void IsExceptionHandler(JSendExceptionHandler handler)
+        {
+            // Exercise system
+            handler.Should().BeAssignableTo<ExceptionHandler>();
+        }
+
+        [Theory, JSendAutoData]
+        public void ConstructorsThrowWhenAnyArgumentIsNull(GuardClauseAssertion assertion)
+        {
+            // Exercise system
+            assertion.Verify(typeof(JSendExceptionHandler).GetConstructors());
+        }
+
+        [Theory, JSendAutoData]
+        public void SetsResultToJSendExceptionResult(IFixture fixture, JSendExceptionHandler handler)
+        {
+            // Fixture setup
+            fixture.Customize<ExceptionContext>(c => c.OmitAutoProperties().With(ctx => ctx.Request));
+            var context = fixture.Create<ExceptionHandlerContext>();
+            // Exercise system
+            handler.Handle(context);
+            // Verify outcome
+            context.Result.Should().BeAssignableTo<JSendExceptionResult>();
+        }
+    }
+}
