f5d1466cfbd7d0e6e958338b4a75033b33b1782d
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-03-05 12:11:41 +0000

    Added JSendAuthorizeAttribute

---
+++ src/JSend.WebApi/JSendAuthorizeAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using JSend.WebApi.Results;
+
+namespace JSend.WebApi
+{
+    /// <summary>
+    /// An authorization filter that verifies the request's <see cref="IPrincipal"/>.
+    /// </summary>
+    [SuppressMessage("Microsoft.Performance", "CA1813:AvoidUnsealedAttributes",
+        Justification = "We want to support extensibility")]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = true, AllowMultiple = true)]
+    public class JSendAuthorizeAttribute : AuthorizeAttribute
+    {
+        /// <summary>
+        /// Processes requests that fail authorization by creating a JSend formatted <see cref="HttpStatusCode.Unauthorized"/> result.
+        /// </summary>
+        /// <param name="actionContext">The context.</param>
+        protected override void HandleUnauthorizedRequest(HttpActionContext actionContext)
+        {
+            if (actionContext == null) throw new ArgumentNullException("actionContext");
+            Contract.Assert(actionContext.ControllerContext != null);
+
+            var formatter = actionContext.ControllerContext.Configuration.GetJsonMediaTypeFormatter();
+            var request = actionContext.Request;
+
+            var result = new JSendUnauthorizedResult(
+                Enumerable.Empty<AuthenticationHeaderValue>(), formatter, request);
+
+            actionContext.Response = result.ExecuteAsync(CancellationToken.None).Result;
+        }
+    }
+}
---
+++ tests/JSend.WebApi.Tests/JSendAuthorizeAttributeResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http.Controllers;
+using FluentAssertions;
+using JSend.WebApi.Tests.FixtureCustomizations;
+using Xunit;
+using Xunit.Extensions;
+
+namespace JSend.WebApi.Tests
+{
+    public class JSendAuthorizeAttributeResult
+    {
+        public class TestableJSendAuthorizeAttribute : JSendAuthorizeAttribute
+        {
+            public override void OnAuthorization(HttpActionContext actionContext)
+            {
+                //skip null-check for actionContext at this stage
+                //in order to verify that HandleUnauthorizedRequest performs a null-check itself
+                base.HandleUnauthorizedRequest(actionContext);
+            }
+        }
+
+        [Theory, JSendAutoData]
+        public void IsAuthorizeAttribute(JSendAuthorizeAttribute attribute)
+        {
+            // Exercise system and verify outcome
+            attribute.Should().BeAssignableTo<JSendAuthorizeAttribute>();
+        }
+
+        [Theory, JSendAutoData]
+        public void ThrowsWhenContextIsNull(TestableJSendAuthorizeAttribute attribute)
+        {
+            // Exercise system and verify outcome
+            Assert.Throws<ArgumentNullException>(() => attribute.OnAuthorization(null));
+        }
+
+        [Theory, JSendAutoData]
+        public void ThrowsWhenControllerHasNoJsonFormatter(HttpActionContext context, JSendAuthorizeAttribute attribute)
+        {
+            // Fixture setup
+            var formatters = context.ControllerContext.Configuration.Formatters;
+            formatters.OfType<JsonMediaTypeFormatter>().ToList()
+                .ForEach(f => formatters.Remove(f));
+
+            var expectedMessage = string.Format("The controller's configuration must contain a formatter of type {0}.",
+                typeof (JsonMediaTypeFormatter).FullName);
+            // Exercise system and verify outcome
+            Action onAuthorization = () => attribute.OnAuthorization(context);
+            onAuthorization.ShouldThrow<ArgumentException>()
+                .And.Message.Should().Contain(expectedMessage);
+        }
+    }
+}
