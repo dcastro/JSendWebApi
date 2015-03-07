98446b149f896a72c77d1c52fe886a8190a1cefa
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-03-07 15:44:54 +0000

    Added build.sh

---
+++ build.sh
@@ -0,0 +1,3 @@
+.nuget/nuget.exe "Install" "xunit.runners" -OutputDirectory packages -ExcludeVersion -version 1.9.2
+.nuget/NuGet.exe "Install" "FAKE" -OutputDirectory packages -ExcludeVersion -version 3.17.14
+packages/FAKE/tools/Fake.exe build.fsx "$@" encoding=utf-8
