4c4429599850c399ef4c23049b4d0e61e9fd413e
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-03-10 07:45:43 +0000

    Added draft of JSendResult<T>

---
+++ src/JSend.Client/JSendResult.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+
+namespace JSend.Client
+{
+    /// <summary>Represents the result of a request to a JSend API.</summary>
+    /// <typeparam name="T">The type of data expected to be returned by the API.</typeparam>
+    public class JSendResult<T>
+    {
+        private readonly IJSendResponse _jsendResponse;
+        private readonly HttpResponseMessage _responseMessage;
+
+        /// <summary>Initializes a new instance of <see cref="JSendResult{T}"/>.</summary>
+        /// <param name="jsendResponse">The JSend response retrieved from the entity body.</param>
+        /// <param name="responseMessage">The HTTP response message.</param>
+        public JSendResult(SuccessResponse<T> jsendResponse, HttpResponseMessage responseMessage)
+        {
+            _responseMessage = responseMessage;
+            _jsendResponse = jsendResponse;
+        }
+        
+        /// <summary>Gets the JSend response retrieved from the entity body.</summary>
+        public IJSendResponse JsendResponse
+        {
+            get { return _jsendResponse; }
+        }
+
+        /// <summary>Gets the HTTP response message.</summary>
+        public HttpResponseMessage ResponseMessage
+        {
+            get { return _responseMessage; }
+        }
+    }
+}
---
+++ tests/JSend.Client.Tests/JSendResultTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using FluentAssertions;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit.Extensions;
+
+namespace JSend.Client.Tests
+{
+    public class JSendResultTests
+    {
+        private class ResponseAutoDataAttribute : AutoDataAttribute
+        {
+            public ResponseAutoDataAttribute()
+                : base(new Fixture().Customize(new AutoConfiguredMoqCustomization()))
+            {
+
+            }
+
+        }
+        
+        [Theory, ResponseAutoData]
+        public void JSendResponseIsCorrectlyInitialized(SuccessResponse<string> jsendResponse,
+            HttpResponseMessage httpResponse)
+        {
+            // Exercise system
+            var result = new JSendResult<string>(jsendResponse, httpResponse);
+            // Verify outcome
+            result.JsendResponse.Should().Be(jsendResponse);
+        }
+
+        [Theory, ResponseAutoData]
+        public void ResponseMessageIsCorrectlyInitialized(SuccessResponse<string> jsendResponse,
+            HttpResponseMessage httpResponse)
+        {
+            // Exercise system
+            var result = new JSendResult<string>(jsendResponse, httpResponse);
+            // Verify outcome
+            result.ResponseMessage.Should().Be(httpResponse);
+        }
+    }
+}
