1744a474be8a122238e89e455e15fdd8a4eaa199
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-03-13 22:38:35 +0000

    Removed deprecated response types

--- src/JSend.Client/IJSendResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JSend.Client
-{
-    /// <summary>A JSend response.</summary>
-    public interface IJSendResponse
-    {
-        /// <summary>Gets the status of this response.</summary>
-        JSendStatus Status { get; }
-    }
-}
