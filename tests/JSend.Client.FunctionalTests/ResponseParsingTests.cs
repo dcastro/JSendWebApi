4fec3a849a967c086b68be71017576e7c3ff3b59
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-03-29 16:44:42 +0100

    Added more response parsing functional tests

--- tests/JSend.Client.FunctionalTests/ResponseParsingTests.cs
+++ tests/JSend.Client.FunctionalTests/ResponseParsingTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using JSend.Client.FunctionalTests.FixtureCustomizations;
 using Xunit.Extensions;
+using Newtonsoft.Json.Linq;
 
 namespace JSend.Client.FunctionalTests
 {
@@ -11,11 +12,56 @@ namespace JSend.Client.FunctionalTests
         public async Task ParsesSuccessResponseWithoutData(JSendClient client)
         {
             // Exercise system
+            using (client)
             using (var response = await client.GetAsync<User>("http://localhost/users/success"))
             {
                 // Verify outcome
                 response.Status.Should().Be(JSendStatus.Success);
                 response.HasData.Should().BeFalse();
+            }
+        }
+
+        [Theory, JSendAutoData]
+        public async Task ParsesSuccessResponseWithData(JSendClient client)
+        {
+            // Exercise system
+            using (client)
+            using (var response = await client.GetAsync<User>("http://localhost/users/success-with-user"))
+            {
+                // Verify outcome
+                response.Status.Should().Be(JSendStatus.Success);
+                response.HasData.Should().BeTrue();
+                response.Data.ShouldBeEquivalentTo(UsersController.TestUser);
+            }
+        }
+
+        [Theory, JSendAutoData]
+        public async Task ParsesFailResponse(JSendClient client)
+        {
+            // Exercise system
+            using (client)
+            using (var response = await client.GetAsync<User>("http://localhost/users/fail"))
+            {
+                // Verify outcome
+                response.Status.Should().Be(JSendStatus.Fail);
+                response.HasData.Should().BeFalse();
+                response.Error.Data.Value<string>().Should().Be(UsersController.ErrorData);
+            }
+        }
+
+        [Theory, JSendAutoData]
+        public async Task ParsesErrorResponse(JSendClient client)
+        {
+            // Exercise system
+            using (client)
+            using (var response = await client.GetAsync<User>("http://localhost/users/error"))
+            {
+                // Verify outcome
+                response.Status.Should().Be(JSendStatus.Error);
+                response.HasData.Should().BeFalse();
+                response.Error.Message.Should().Be(UsersController.ErrorMessage);
+                response.Error.Code.Should().Be(UsersController.ErrorCode);
+                response.Error.Data.Value<string>().Should().Be(UsersController.ErrorData);
             }
         }
     }
--- tests/JSend.Client.FunctionalTests/UsersController.cs
+++ tests/JSend.Client.FunctionalTests/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Net;
+using System.Web.Http;
 using JSend.WebApi;
 
 namespace JSend.Client.FunctionalTests
@@ -6,10 +7,33 @@ namespace JSend.Client.FunctionalTests
     [RoutePrefix("users")]
     public class UsersController : JSendApiController
     {
+        public static readonly User TestUser = new User {Username = "DCastro"};
+        public static readonly string ErrorMessage = "dummy error message";
+        public static readonly int ErrorCode = 80;
+        public static readonly string ErrorData = "dummy error data";
+
         [Route("success"), HttpGet]
         public void SuccessAction()
         {
 
+        }
+
+        [Route("success-with-user"), HttpGet]
+        public User SuccessWithUserAction()
+        {
+            return TestUser;
+        }
+
+        [Route("fail"), HttpGet]
+        public IHttpActionResult FailAction()
+        {
+            return JSendFail(HttpStatusCode.BadRequest, ErrorData);
+        }
+
+        [Route("error"), HttpGet]
+        public IHttpActionResult ErrorAction()
+        {
+            return JSendError(HttpStatusCode.InternalServerError, ErrorMessage, ErrorCode, ErrorData);
         }
     }
 }
