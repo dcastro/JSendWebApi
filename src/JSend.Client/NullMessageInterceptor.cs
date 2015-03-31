5e908f05f63685f585f6e83b2f4bef21600cc14b
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-03-31 21:56:32 +0100

    Added NullMessageInterceptor

---
+++ src/JSend.Client/NullMessageInterceptor.cs
@@ -0,0 +1,14 @@
+﻿namespace JSend.Client
+{
+    /// <summary>Message interceptor that does nothing.</summary>
+    public sealed class NullMessageInterceptor : MessageInterceptor
+    {
+        private static readonly NullMessageInterceptor _instance = new NullMessageInterceptor();
+
+        /// <summary>Gets an instance of <see cref="NullMessageInterceptor"/>.</summary>
+        public static NullMessageInterceptor Instance
+        {
+            get { return _instance; }
+        }
+    }
+}
---
+++ tests/JSend.Client.Tests/NullMessageInterceptorTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace JSend.Client.Tests
+{
+    public class NullMessageInterceptorTests
+    {
+        [Fact]
+        public void InstanceIsSingleton()
+        {
+            // Exercise system
+            var first = NullMessageInterceptor.Instance;
+            var second = NullMessageInterceptor.Instance;
+            // Verify outcome
+            first.Should().BeSameAs(second);
+        }
+    }
+}
