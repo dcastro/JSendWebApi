84f2b4956ecf362e60ed60d645dac0d17c2c4953
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-03-31 21:56:36 +0100

    Added MessageInterceptor property to JSendClientSettings

--- src/JSend.Client/JSendClientSettings.cs
+++ src/JSend.Client/JSendClientSettings.cs
@@ -11,6 +11,9 @@ namespace JSend.Client
         /// <summary>Gets or sets the parser used to process JSend-formatted responses.</summary>
         public IJSendParser Parser { get; set; }
 
+        /// <summary>Gets or sets the interceptor to perform additional work with outgoing requests/incoming responses.</summary>
+        public MessageInterceptor MessageInterceptor { get; set; }
+
         /// <summary>Gets or sets the encoding used to format a request's content.</summary>
         public Encoding Encoding { get; set; }
 
---
+++ tests/JSend.Client.Tests/JSendClientSettingsTests.cs
@@ -0,0 +1,16 @@
+﻿using JSend.Client.Tests.FixtureCustomizations;
+using Ploeh.AutoFixture.Idioms;
+using Xunit.Extensions;
+
+namespace JSend.Client.Tests
+{
+    public class JSendClientSettingsTests
+    {
+        [Theory, JSendAutoData]
+        public void WritablePropertiesBehaveCorrectly(WritablePropertyAssertion assertion)
+        {
+            // Exercise system and verify outcome
+            assertion.Verify(typeof (JSendClientSettings).GetProperties());
+        }
+    }
+}
