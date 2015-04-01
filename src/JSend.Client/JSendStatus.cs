993bdfd032508d6a37d33302999fb5d9a92baf46
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-04-01 22:31:50 +0100

    Fixed typo in xml docs

--- src/JSend.Client/JSendStatus.cs
+++ src/JSend.Client/JSendStatus.cs
@@ -7,7 +7,8 @@
         Success,
         /// <summary>
         /// Indicates that there was a problem with the data submitted,
-        /// or some pre-condition of the API call wasn't satisfied.</summary>
+        /// or some pre-condition of the API call wasn't satisfied.
+        /// </summary>
         Fail,
         /// <summary>An error occurred in processing the request, i.e. an exception was thrown.</summary>
         Error
