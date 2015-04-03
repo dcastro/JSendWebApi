d016e0bbe59e804dff5995f5bc93a2a9479b364a
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-04-03 14:04:27 +0100

    Added CompositeMessageInterceptor

---
+++ src/JSend.Client/CompositeMessageInterceptor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace JSend.Client
+{
+    /// <summary>
+    /// Performs additional work with outgoing requests/incoming responses by invoking all
+    /// contained <see cref="Interceptors"/>.
+    /// </summary>
+    public class CompositeMessageInterceptor : MessageInterceptor
+    {
+        private readonly IEnumerable<MessageInterceptor> _interceptors;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="CompositeMessageInterceptor"/>.
+        /// </summary>
+        /// <param name="interceptors">The message interceptors.</param>
+        public CompositeMessageInterceptor(IEnumerable<MessageInterceptor> interceptors)
+            : this(interceptors.ToArray())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="CompositeMessageInterceptor"/>.
+        /// </summary>
+        /// <param name="interceptors">The message interceptors.</param>
+        public CompositeMessageInterceptor(params MessageInterceptor[] interceptors)
+        {
+            if (interceptors == null) throw new ArgumentNullException("interceptors");
+
+            _interceptors = interceptors;
+        }
+
+        /// <summary>Gets the interceptors contained within this instance.</summary>
+        public IEnumerable<MessageInterceptor> Interceptors
+        {
+            get { return _interceptors; }
+        }
+
+        /// <summary>
+        /// Invokes <see cref="MessageInterceptor.OnSending"/> on the contained <see cref="Interceptors"/>.
+        /// </summary>
+        /// <param name="request">A request that is about to be dispatched.</param>
+        public override void OnSending(HttpRequestMessage request)
+        {
+            foreach (var interceptor in Interceptors)
+                interceptor.OnSending(request);
+        }
+
+        /// <summary>
+        /// Invokes <see cref="MessageInterceptor.OnReceived"/> on the contained <see cref="Interceptors"/>.
+        /// </summary>
+        /// <param name="context">The contextual data associated with the HTTP response message.</param>
+        public override void OnReceived(ResponseReceivedContext context)
+        {
+            foreach (var interceptor in Interceptors)
+                interceptor.OnReceived(context);
+        }
+
+        /// <summary>
+        /// Invokes <see cref="MessageInterceptor.OnParsed{TResponse}"/> on the contained <see cref="Interceptors"/>.
+        /// </summary>
+        /// <typeparam name="TResponse">The type of the data contained by the JSend response.</typeparam>
+        /// <param name="context">The contextual data associated with the parsing.</param>
+        public override void OnParsed<TResponse>(ResponseParsedContext<TResponse> context)
+        {
+            foreach (var interceptor in Interceptors)
+                interceptor.OnParsed(context);
+        }
+
+        /// <summary>
+        /// Invokes <see cref="MessageInterceptor.OnException"/> on the contained <see cref="Interceptors"/>.
+        /// </summary>
+        /// <param name="context">The contextual data associated the exception.</param>
+        public override void OnException(ExceptionContext context)
+        {
+            foreach (var interceptor in Interceptors)
+                interceptor.OnException(context);
+        }
+    }
+}
---
+++ tests/JSend.Client.Tests/CompositeMessageInterceptorTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using FluentAssertions;
+using JSend.Client.Tests.FixtureCustomizations;
+using Moq;
+using Ploeh.AutoFixture.Idioms;
+using Xunit.Extensions;
+
+namespace JSend.Client.Tests
+{
+    public class CompositeMessageInterceptorTests
+    {
+        [Theory, JSendAutoData]
+        public void ArgumentsCannotBeNull(GuardClauseAssertion assertion)
+        {
+            // Exercise system and verify  outcome
+            assertion.Verify(typeof (CompositeMessageInterceptor).GetConstructors());
+        }
+
+        [Theory, JSendAutoData]
+        public void Interceptors_AreCorrectlyInitialized_UsingAnArray(MessageInterceptor[] interceptors)
+        {
+            // Exercise system
+            var composite = new CompositeMessageInterceptor(interceptors);
+            // Verify outcome
+            composite.Interceptors.Should().BeEquivalentTo(interceptors);
+        }
+
+        [Theory, JSendAutoData]
+        public void Interceptors_AreCorrectlyInitialized_UsingAnEnumerable(List<MessageInterceptor> interceptors)
+        {
+            // Exercise system
+            var composite = new CompositeMessageInterceptor(interceptors);
+            // Verify outcome
+            composite.Interceptors.Should().BeEquivalentTo(interceptors);
+        }
+
+        [Theory, JSendAutoData]
+        public void InvokesOnSending_OnAllInterceptors(HttpRequestMessage request, CompositeMessageInterceptor composite)
+        {
+            // Exercise system
+            composite.OnSending(request);
+            // Verify outcome
+            foreach (var interceptor in composite.Interceptors)
+                Mock.Get(interceptor)
+                    .Verify(i => i.OnSending(request));
+        }
+
+        [Theory, JSendAutoData]
+        public void InvokesOnReceived_OnAllInterceptors(
+            ResponseReceivedContext context, CompositeMessageInterceptor composite)
+        {
+            // Exercise system
+            composite.OnReceived(context);
+            // Verify outcome
+            foreach (var interceptor in composite.Interceptors)
+                Mock.Get(interceptor)
+                    .Verify(i => i.OnReceived(context));
+        }
+
+        [Theory, JSendAutoData]
+        public void InvokesOnParsed_OnAllInterceptors(
+            ResponseParsedContext<object> context, CompositeMessageInterceptor composite)
+        {
+            // Exercise system
+            composite.OnParsed(context);
+            // Verify outcome
+            foreach (var interceptor in composite.Interceptors)
+                Mock.Get(interceptor)
+                    .Verify(i => i.OnParsed(context));
+        }
+
+        [Theory, JSendAutoData]
+        public void InvokesOnException_OnAllInterceptors(ExceptionContext context, CompositeMessageInterceptor composite)
+        {
+            // Exercise system
+            composite.OnException(context);
+            // Verify outcome
+            foreach (var interceptor in composite.Interceptors)
+                Mock.Get(interceptor)
+                    .Verify(i => i.OnException(context));
+        }
+    }
+}
