be1dba54e5e6178ebd279dfa709a6e7f3b832fc0
Diogo Castro <diogo.filipe.acastro@gmail.com>
2015-04-10 12:10:22 +0100

    Minor code cleanup

--- src/JSend.Client/Properties/AssemblyInfo.cs
+++ src/JSend.Client/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Resources;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following 
