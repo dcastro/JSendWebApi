e428bb5a881f09ed41ca20a501ff80a86c409214
Diogo Castro <dc@diogocastro.com>
2015-05-23 11:49:36 +0100

    Extracted StatusCode and Request properties into IJSendResult<T>

--- src/JSend.WebApi/IJSendResult.cs
+++ src/JSend.WebApi/IJSendResult.cs
@@ -1,11 +1,23 @@
-﻿using System.Web.Http;
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using JSend.WebApi.Responses;
 
 namespace JSend.WebApi
 {
+    /// <summary>
+    /// Represents an action result that returns the specified JSend response with the specified status code.
+    /// </summary>
+    /// <typeparam name="TResponse">The type of the JSend response.</typeparam>
     public interface IJSendResult<out TResponse> : IHttpActionResult where TResponse : IJSendResponse
     {
         /// <summary>Gets the response to be formatted into the message's body.</summary>
         TResponse Response { get; }
+
+        /// <summary>Gets the HTTP status code for the response message.</summary>
+        HttpStatusCode StatusCode { get; }
+
+        /// <summary>Gets the request message which led to this result.</summary>
+        HttpRequestMessage Request { get; }
     }
 }
